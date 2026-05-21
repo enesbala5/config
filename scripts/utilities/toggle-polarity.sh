9b3afcd131aaae6c7dda03d03e1f0c43ce7a5d1c
Enes Bala <contact@enesbala.com>
2026-05-21 14:52:23 +0200

    Add wallpaper update at toggle-polarity script

--- scripts/utilities/toggle-polarity.sh
+++ scripts/utilities/toggle-polarity.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
+
 DUMMY_FILE="/tmp/light_mode_active"
+
+LIGHT_MODE_WALLPAPER="~/config/wallpapers/distortion-1-inverted.png"
+DARK_MODE_WALLPAPER="~/config/wallpapers/distortion-1.png"
+
 if [ -f "$DUMMY_FILE" ]; then
   sudo /nix/var/nix/profiles/system/bin/switch-to-configuration test
+  waypaper --wallpaper "$DARK_MODE_WALLPAPER"
+  notify-send "🌙 Switched to Dark Mode"
   rm -f "$DUMMY_FILE"
 else
   sudo /run/current-system/specialisation/light/bin/switch-to-configuration test
+  waypaper --wallpaper "$LIGHT_MODE_WALLPAPER"
+  notify-send "☀️ Switched to Light Mode"
   touch "$DUMMY_FILE"
 fi
 
