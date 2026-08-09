b08271c788cce1e91b1bd8373ae28fc78244c4e2
Enes Bala <contact@enesbala.com>
2026-08-10 01:27:41 +0200

    Fix path to Toshiba HDD at home-server hardware config

--- nix/nixos/hosts/home-server/hardware-configuration.nix
+++ nix/nixos/hosts/home-server/hardware-configuration.nix
@@ -28,11 +28,11 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/b/by-uuid/d8bd78ce-5675-4efd-a684-9ca2ee70c895";
+      device = "/dev/disk/by-uuid/d8bd78ce-5675-4efd-a684-9ca2ee70c895";
       fsType = "ext4";
     };
     "/mnt/hdd" = {
-	device = "/dev/disk-by-uuid/b5ffe500-98f7-4214-a27c-2db9d7e37ed1";
+	device = "/dev/disk/by-uuid/b5ffe500-98f7-4214-a27c-2db9d7e37ed1";
 	fsType = "ext4";
 	options = [ "noatime" "nodiratime" "nofail" ];
     };
