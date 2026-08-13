266b252052e2edf8c2a42d5891ff1dced3243d58
Enes Bala <contact@enesbala.com>
2026-08-13 02:43:43 +0200

    Enable BIOS GRUB so home-server boots new generations
    - Re-enable GRUB on the system NVMe MBR so nixos-rebuild can update the boot entry
    - Pin the install device by-id so GRUB is not written to the data HDD

--- nix/nixos/hosts/home-server/default.nix
+++ nix/nixos/hosts/home-server/default.nix
@@ -847,10 +847,11 @@ in
 
   boot = {
     loader = {
-      # efi.canTouchEfiVariables = true;
-
       grub = {
-        enable = false;
+        enable = true;
+        # BIOS/MBR (single msdos partition, no ESP). by-id pins this to the
+        # system NVMe so GRUB is not installed to the data HDD.
+        device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_500GB_S4EVNX1W425280F";
       };
     };
   };
