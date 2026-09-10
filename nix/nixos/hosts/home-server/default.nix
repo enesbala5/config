9aaf7cff854e6f27676132bccfe546d012806a7a
Enes Bala <contact@enesbala.com>
2026-09-10 12:13:22 +0200

    Update tailscale package on home-server

--- nix/nixos/hosts/home-server/default.nix
+++ nix/nixos/hosts/home-server/default.nix
@@ -217,7 +217,7 @@ in
     avahi = {
       publish.enable = true;
       publish.userServices = true;
-      
+
       # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
       nssmdns4 = true;
       # ^^ Not one hundred percent sure if this is needed- if it aint broke, don't fix it
@@ -225,7 +225,10 @@ in
       openFirewall = true;
     };
 
-    tailscale.enable = true;
+    tailscale = {
+      enable = true;
+      package = unstable.tailscale;
+    };
 
     fail2ban.enable = true;
   };
